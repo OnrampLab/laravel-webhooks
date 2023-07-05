b9c68ba730bb0297c9417dc0c502710228865c98
Luna <luna.moon.brillar@gmail.com>
2023-07-05 18:07:26 +0800

    chore(concern): [Webhookable] create trait

---
+++ src/Concerns/Webhookable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OnrampLab\Webhooks\Concerns;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use OnrampLab\Webhooks\Models\Webhook;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+/**
+ * @mixin Model
+ */
+trait Webhookable
+{
+    public function getEventOccurredAt(): ?Carbon
+    {
+        return now();
+    }
+
+    public function getWebhookContext(): ?Model
+    {
+        return null;
+    }
+
+    public function getWebhookPayload(): array
+    {
+        return [];
+    }
+}
