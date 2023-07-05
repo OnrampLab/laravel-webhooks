fa6f4d533b369b170b4272dd1ded167631221ba0
Luna <luna.moon.brillar@gmail.com>
2023-07-05 18:03:33 +0800

    chore(migration): [webhookLogs] create table

---
+++ database/migrations/2023_07_04_011527_create_webhook_logs_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('webhook_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('webhook_id');
+            $table->timestamp('event_occurred_at');
+            $table->string('endpoint');
+            $table->json('request_body');
+            $table->timestamp('sent_at');
+            $table->timestamp('received_at')->nullable();
+            $table->text('response')->nullable();
+            $table->string('status_code')->nullable();
+            $table->decimal('execution_time', 10, 4)->nullable();
+            $table->text('error_type')->nullable();
+            $table->text('error_message')->nullable();
+            $table->foreign('webhook_id')->references('id')->on('webhooks')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('webhook_logs');
+    }
+};
