1c0cda9535dbccd58cc8b2daa1ad00cbb87a75f2
Luna <luna.moon.brillar@gmail.com>
2023-07-05 18:07:39 +0800

    chore(contract): [Webhookable] create interface

---
+++ src/Contracts/Webhookable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OnrampLab\Webhooks\Contracts;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+interface Webhookable
+{
+    public function getWebhookPayload(): array;
+
+    public function getWebhookContext(): ?Model;
+
+    public function getEventOccurredAt(): ?Carbon;
+}
