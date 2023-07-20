63646ac649d5cdbdc2c5c39eb4460be132b314aa
Luna <luna.moon.brillar@gmail.com>
2023-07-20 14:17:41 +0800

    chore(migration): modify create_webhooks_table

--- database/migrations/2023_07_04_010529_create_webhooks_table.php
+++ database/migrations/2023_07_04_010529_create_webhooks_table.php
@@ -15,15 +15,15 @@ return new class extends Migration
     {
         Schema::create('webhooks', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->string('endpoint');
             $table->string('http_verb')->default('POST');
-            $table->boolean('enabled');
-            $table->json('exclusion_criteria');
+            $table->boolean('enabled')->default(true);
+            $table->json('exclusion_criteria')->nullable();
             $table->unsignedBigInteger('contextable_id')->nullable();
             $table->string('contextable_type')->nullable();
             $table->json('headers')->nullable();
             $table->string('secret')->nullable();
-            $table->timestamps();
         });
     }
 
